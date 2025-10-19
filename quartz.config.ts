d971ba2bb40d8c572729ef399246a05d431f1b00
Abibullaev Suleyman <sabibulaev25@gmail.com>
2025-10-19 11:06:10 +0300

    Quartz sync: Oct 19, 2025, 11:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Сулейман",
+    pageTitle: "WhiteSun13",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
